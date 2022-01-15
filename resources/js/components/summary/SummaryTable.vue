650b7e52d5b24037d563dc113784aa5a0119c514
Mona Lisa <exxxar@gmail.com>
2022-01-16 00:56:22 +0300

    first

--- resources/js/components/summary/SummaryTable.vue
+++ resources/js/components/summary/SummaryTable.vue
@@ -330,18 +330,35 @@ export default {
 
                 tmp.forEach(item => {
                     if (item !== "date" && item !== "average" && item !== "total") {
-                        if (this.records[i][item].device_type_id === 2)
+                        if (this.records[i][item].device_type_id === 2) {
+                            console.log(item,"=>",this.records[i][item])
                             names_2[item] =  this.records[i][item]
+                            console.log("names_2=>",names_2)
+                        }
+
+
 
 
                         if (this.records[i][item].device_type_id === 3)
+                        {
+                            console.log(item,"=>",this.records[i][item])
                             names_3[item] =  this.records[i][item]
+                            console.log("names_3=>",names_2)
+                        }
 
                         if (this.records[i][item].device_type_id === 5)
+                        {
+                            console.log(item,"=>",this.records[i][item])
                             names_5[item] =  this.records[i][item]
+                            console.log("names_5=>",names_2)
+                        }
                     }
                     else
+                    {
+                        console.log(item,"=>",this.records[i][item])
                         other[item] =  this.records[i][item]
+                        console.log("other=>",other)
+                    }
                 });
 
                 data[i] =  [...names_2, ...names_5, ...names_3, ...other];
